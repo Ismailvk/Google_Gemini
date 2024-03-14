0c5c57444b8375d39988e625dfdc7fae3ae4a84b
Ismailvk <ismailkbassi00@gmail.com>
2024-03-14 11:53:00 +0530

    updated ai images

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,8 +14,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.paddingOf(context).top;
     return Scaffold(
-      appBar: AppBar(),
       body: BlocConsumer<ChatBloc, ChatState>(
         bloc: chatBloc,
         listener: (context, state) {},
@@ -24,9 +24,27 @@ class HomeScreen extends StatelessWidget {
             case ChatSuccessState:
               List<ChatMessageModel> message =
                   (state as ChatSuccessState).message;
-              return SafeArea(
+              return Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image:
+                        AssetImage('assets/Artificial intelligence (AI).jpeg'),
+                    fit: BoxFit.cover,
+                    opacity: 0.4,
+                  ),
+                ),
                 child: Column(
                   children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: size, left: 20),
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          'G E M I N I',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
                     Expanded(
                         child: ListView.builder(
                       itemCount: message.length,
@@ -34,11 +52,67 @@ class HomeScreen extends StatelessWidget {
                         return Padding(
                           padding: const EdgeInsets.all(5),
                           child: Container(
-                              alignment: message[index].role == 'user'
-                                  ? Alignment.topLeft
-                                  : Alignment.topRight,
-                              color: Colors.grey.withOpacity(0.4),
-                              child: Text(message[index].parts.first.text)),
+                            alignment: message[index].role == 'user'
+                                ? Alignment.topRight
+                                : Alignment.topLeft,
+                            child: Row(
+                              mainAxisAlignment: message[index].role == 'user'
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                message[index].role == 'user'
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey.withOpacity(0.2),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8),
+                                          child: Text(
+                                            message[index].parts.first.text,
+                                            style:
+                                                const TextStyle(fontSize: 17),
+                                            textAlign: TextAlign.end,
+                                          ),
+                                        ),
+                                      )
+                                    : SizedBox(
+                                        height: 35,
+                                        width: 35,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(40),
+                                          child: Image.network(
+                                            'https://logowik.com/content/uploads/images/google-ai-gemini91216.logowik.com.webp',
+                                            fit: BoxFit.fitHeight,
+                                          ),
+                                        ),
+                                      ),
+                                const SizedBox(width: 8),
+                                message[index].role == 'user'
+                                    ? const CircleAvatar(
+                                        radius: 20, child: Text('You'))
+                                    : Expanded(
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color:
+                                                  Colors.grey.withOpacity(0.2),
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8),
+                                            child: Text(
+                                              message[index].parts.first.text,
+                                              style:
+                                                  const TextStyle(fontSize: 17),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                              ],
+                            ),
+                          ),
                         );
                       },
                     )),
@@ -55,14 +129,16 @@ class HomeScreen extends StatelessWidget {
                               padding: const EdgeInsets.all(8.0),
                               child: TextField(
                                 controller: textEditingController,
-                                decoration: const InputDecoration(
+                                decoration: InputDecoration(
                                     focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
                                       borderSide:
-                                          BorderSide(color: Colors.white),
+                                          const BorderSide(color: Colors.white),
                                     ),
                                     enabledBorder: OutlineInputBorder(
-                                        borderSide:
-                                            BorderSide(color: Colors.white)),
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                            color: Colors.white)),
                                     hintText: 'Type here .....'),
                               ),
                             ),
@@ -81,9 +157,16 @@ class HomeScreen extends StatelessWidget {
                                         ChatNewMessageEvent(newChat: text));
                                   }
                                 },
-                                style: const ButtonStyle(
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                    ),
                                     backgroundColor:
-                                        MaterialStatePropertyAll(Colors.white)),
+                                        const MaterialStatePropertyAll(
+                                            Colors.white)),
                                 child:
                                     const Icon(Icons.send, color: Colors.black),
                               ),
